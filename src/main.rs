ca7349472377dbabb3e8fe07555cde158dd5af1e
Ludo Galabru <ludovic@galabru.com>
2020-05-27 23:24:36 -0400

    Naive prototype, displaying the result of the analysis

--- src/main.rs
+++ src/main.rs
@@ -9,67 +9,11 @@
 #[macro_use] extern crate lazy_static;
 
 pub mod clarity;
-use clap::Clap;
+pub mod repl;
 
-/// This doc string acts as a help message when the user runs '--help'
-/// as do all doc strings on fields
-#[derive(Clap)]
-#[clap(version = "1.0")]
-struct Opts {
-    /// Sets a custom config file. Could have been an Option<T> with no default too
-    #[clap(short = "c", long = "config", default_value = "default.conf")]
-    config: String,
-    /// Some input. Because this isn't an Option<T> it's required to be used
-    input: String,
-    /// A level of verbosity, and can be used multiple times
-    #[clap(short = "v", long = "verbose", parse(from_occurrences))]
-    verbose: i32,
-    #[clap(subcommand)]
-    subcmd: SubCommand,
-}
-
-#[derive(Clap)]
-enum SubCommand {
-    /// A help message for the Test subcommand
-    #[clap(name = "test", version = "1.3", author = "Someone Else")]
-    Test(Test),
-}
-
-/// A subcommand for controlling testing
-#[derive(Clap)]
-struct Test {
-    /// Print debug info
-    #[clap(short = "d")]
-    debug: bool
-}
+use repl::Session;
 
 fn main() {
-    let opts: Opts = Opts::parse();
-
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    println!("Value for config: {}", opts.config);
-    println!("Using input file: {}", opts.input);
-
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match opts.verbose {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
-
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
-    match opts.subcmd {
-        SubCommand::Test(t) => {
-            if t.debug {
-                println!("Printing debug info...");
-            } else {
-                println!("Printing normally...");
-            }
-        }
-    }
-
-    // more program logic goes here...
+    let mut session = Session::new();
+    session.start();
 }
