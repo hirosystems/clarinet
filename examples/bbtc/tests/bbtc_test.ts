e6f77450694ed3e74e98df5e0d36138dcae82381
Ludo Galabru <ludovic@blockstack.com>
2021-03-16 00:14:28 -0400

    update test layout

--- examples/bbtc/tests/bbtc_test.ts
+++ examples/bbtc/tests/bbtc_test.ts
@@ -1,22 +1,22 @@
-import { Clarinet, Block, Chain, Account } from './index.ts';
+import { Clarinet, Block, Chain, Account } from 'https://deno.land/x/clarinet@v0.1.1/index.ts';
 
 Clarinet.test({
     name: "Ensure that test 1 are being executed",
     async fn(chain: Chain, accounts: Array<Account>) {
-        console.log(`Hello ${JSON.stringify(chain)} and accounts ${JSON.stringify(accounts)}`);
+        console.log(`Test initialized with Chain::${chain.sessionId} and accounts ${JSON.stringify(accounts)}`);
     },
 });
 
 Clarinet.test({
     name: "Ensure that test 2 are being executed",
-    async fn(chain: any, accounts: any) {
-        // console.log(`Hello ${chain} and accounts ${accounts}`);
+    async fn(chain: Chain, accounts: Array<Account>) {
+        console.log(`Test initialized with Chain::${chain.sessionId} and accounts ${JSON.stringify(accounts)}`);
     },
 });
 
 Clarinet.test({
     name: "Ensure that tests 3 are being executed",
-    async fn(chain: any, accounts: any) {
-        // console.log(`Hello ${chain} and accounts ${accounts}`);
+    async fn(chain: Chain, accounts: Array<Account>) {
+        console.log(`Test initialized with Chain::${chain.sessionId} and accounts ${JSON.stringify(accounts)}`);
     },
 });
