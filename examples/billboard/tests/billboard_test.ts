56b664f3a32e97ec59f4ae8b587e7ac29002e09b
Ludo Galabru <ludovic@blockstack.com>
2021-06-22 21:46:42 -0400

    fix: billboard test

--- examples/billboard/tests/billboard_test.ts
+++ examples/billboard/tests/billboard_test.ts
@@ -1,8 +1,8 @@
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.6.0/index.ts';
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.12.0/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "A quick demo on how to assert expectations",
     async fn(chain: Chain, accounts: Map<string, Account>) {
         let wallet_1 = accounts.get('wallet_1')!;
 
@@ -25,6 +25,12 @@ Clarinet.test({
         block.receipts[3].result
             .expectUtf8('testing...');
         
+        let [event] = block.receipts[0].events;
+        let {sender, recipient, amount} = event.stx_transfer_event;
+        sender.expectPrincipal("ST1J4G6RR643BCG8G8SR6M2D9Z9KXT2NJDRK3FBTK");
+        recipient.expectPrincipal("ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.billboard");
+        amount.expectInt(100);
+
         assetMaps = chain.getAssetsMaps();
         assertEquals(assetMaps.assets['STX'][wallet_1.address], balance - 210);
     },
