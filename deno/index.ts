bb1c5c7de89622fc04fff85172780d676501cfa7
Ludo Galabru <ludovic@blockstack.com>
2021-11-19 13:42:52 -0500

    fix: error message

--- deno/index.ts
+++ deno/index.ts
@@ -616,7 +616,7 @@ Array.prototype.expectSTXTransferEvent = function (
       continue;
     }
   }
-  throw new Error(`Unable to retrieve expected NonFungibleTokenTransferEvent`);
+  throw new Error(`Unable to retrieve expected STXTransferEvent`);
 };
 
 Array.prototype.expectFungibleTokenTransferEvent = function (
