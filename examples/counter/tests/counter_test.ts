354fecf1dbc9fbc4ef47996b7710a631667596a6
Ludo Galabru <ludovic@galabru.com>
2022-03-30 11:53:08 -0400

    feat: use lib v0.28.0

--- examples/counter/tests/counter_test.ts
+++ examples/counter/tests/counter_test.ts
@@ -1,4 +1,4 @@
-import { Clarinet, Tx, Chain, Account, Contract, types } from 'https://deno.land/x/clarinet@v0.27.0/index.ts';
+import { Clarinet, Tx, Chain, Account, Contract, types } from 'https://deno.land/x/clarinet@v0.28.0/index.ts';
 import { assertEquals } from "https://deno.land/std@0.90.0/testing/asserts.ts";
 
 Clarinet.test({
@@ -54,29 +54,29 @@ Clarinet.test({
     },
 });
 
-// Clarinet.test({
-//     name: "Test with pre-setup",
-//     beforeContractsDeployment: async (chain: Chain, accounts: Map<string, Account>) => {
-//         chain.mineEmptyBlock(100);
-//     },
-//     async fn(chain: Chain, accounts: Map<string, Account>, contracts: Map<string, Contract>) {
-//         let wallet_1 = accounts.get("wallet_1")!;
-//         let wallet_2 = accounts.get("wallet_2")!;
+Clarinet.test({
+    name: "Test with pre-setup",
+    beforeContractsDeployment: async (chain: Chain, accounts: Map<string, Account>) => {
+        chain.mineEmptyBlock(100);
+    },
+    async fn(chain: Chain, accounts: Map<string, Account>, contracts: Map<string, Contract>) {
+        let wallet_1 = accounts.get("wallet_1")!;
+        let wallet_2 = accounts.get("wallet_2")!;
 
-//         let block = chain.mineBlock([
-//             Tx.contractCall("counter", "increment", [types.uint(1)], wallet_1.address),
-//             Tx.contractCall("counter", "increment", [types.uint(4)], wallet_1.address),
-//             Tx.contractCall("counter", "increment", [types.uint(10)], wallet_1.address)
-//         ]);
-//         assertEquals(block.height, 102);
-//         block.receipts[0].result
-//             .expectOk()
-//             .expectUint(2);
-//         block.receipts[1].result
-//             .expectOk()
-//             .expectUint(6);
-//         block.receipts[2].result
-//             .expectOk()
-//             .expectUint(16);
-//     },
-// });
+        let block = chain.mineBlock([
+            Tx.contractCall("counter", "increment", [types.uint(1)], wallet_1.address),
+            Tx.contractCall("counter", "increment", [types.uint(4)], wallet_1.address),
+            Tx.contractCall("counter", "increment", [types.uint(10)], wallet_1.address)
+        ]);
+        assertEquals(block.height, 102);
+        block.receipts[0].result
+            .expectOk()
+            .expectUint(2);
+        block.receipts[1].result
+            .expectOk()
+            .expectUint(6);
+        block.receipts[2].result
+            .expectOk()
+            .expectUint(16);
+    },
+});
