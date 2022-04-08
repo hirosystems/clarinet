c03d223cbe5336e767206fc197cb05f5ace137d5
Callum McIntyre <callum@pointer.gg>
2022-04-08 13:50:40 +0100

    docs: change clarinet deploy to clarinet contract publish

--- src/frontend/cli.rs
+++ src/frontend/cli.rs
@@ -985,13 +985,13 @@ fn display_deploy_hint() {
         yellow!("Once your contracts are ready to be deployed, you can run the following:")
     );
 
-    println!("{}", blue!("  $ clarinet deploy --testnet"));
+    println!("{}", blue!("  $ clarinet contract publish --testnet"));
     println!(
         "{}",
         yellow!("    Deploy all contracts to the testnet network.\n")
     );
 
-    println!("{}", blue!("  $ clarinet deploy --mainnet"));
+    println!("{}", blue!("  $ clarinet contract publish --mainnet"));
     println!(
         "{}",
         yellow!("    Deploy all contracts to the mainnet network.\n")
