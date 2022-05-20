1183ba7f707b555a21840f3777ed86b543d629a7
Ludo Galabru <ludovic@galabru.com>
2022-05-20 14:28:52 -0400

    Update src/deployment/types.rs
    Co-authored-by: Brice <brice@hiro.so>

--- src/deployment/types.rs
+++ src/deployment/types.rs
@@ -285,7 +285,7 @@ impl EmulatedContractPublishSpecification {
             Ok(res) => res,
             Err(_) => {
                 return Err(format!(
-                    "unable to use {} as a valid contract name",
+                    "unable to parse '{}' as a valid contract name",
                     specs.contract_name
                 ))
             }
