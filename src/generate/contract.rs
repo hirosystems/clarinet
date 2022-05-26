acb1f846ec82ec03ea669860bf6b50f2071ce97f
Brice Dobry <brice@hiro.so>
2022-05-26 07:46:53 -0400

    fix: use `clarinet@0.31.0` lib in generated tests
    Resolves: #381

--- src/generate/contract.rs
+++ src/generate/contract.rs
@@ -72,7 +72,7 @@ impl GetChangesForNewContract {
     fn create_template_test(&mut self) {
         let content = format!(
             r#"
-import {{ Clarinet, Tx, Chain, Account, types }} from 'https://deno.land/x/clarinet@v0.28.0/index.ts';
+import {{ Clarinet, Tx, Chain, Account, types }} from 'https://deno.land/x/clarinet@v0.31.0/index.ts';
 import {{ assertEquals }} from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({{
