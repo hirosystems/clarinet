55f402d3d45ec71fa9c70b9e6c79629bf71803ba
Ludo Galabru <ludo@hiro.so>
2022-06-01 10:14:16 -0400

    feat: add cbtc example

---
+++ examples/cbtc/backend/handler.ts
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports.wrapBtc = async (event: any) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.unwrapBtc = async (event: any) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
