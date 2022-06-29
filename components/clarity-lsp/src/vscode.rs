7ad8d2fada7f080ae633c24a9c840da3c0156ef3
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2022-06-29 19:56:50 +0200

    chore: refacto

--- components/clarity-lsp/src/vscode.rs
+++ components/clarity-lsp/src/vscode.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, mem::take};
 
 use crate::utils::convert_clarity_diagnotic_to_lsp_diagnostic;
 use clarinet_deployments::generate_simnet_deployment_for_snippet;
@@ -15,13 +15,14 @@ use lsp_types::{
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen_futures::{spawn_local, JsFuture};
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 }
+
 #[derive(Serialize, Deserialize)]
 pub struct VFSRequest {
     pub path: String,
@@ -33,31 +34,42 @@ pub struct FileEvent {
 }
 
 #[derive(Serialize, Deserialize)]
+pub struct ASTEvent {
+    pub path: String,
+    pub ast: String,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct CursorEvent {
     pub path: String,
     pub line: u32,
     pub char: u32,
 }
 
+#[derive(Default)]
 #[wasm_bindgen]
 pub struct ClarityLanguageServer {
+    manifest: Option<String>,
     asts: BTreeMap<String, ContractAST>,
     client_diagnostic_tx: js_sys::Function,
-    _client_request_tx: js_sys::Function,
+    _client_notification_tx: js_sys::Function,
+    client_request_tx: js_sys::Function,
 }
 
-/// Public API exposed via WASM.
 #[wasm_bindgen]
 impl ClarityLanguageServer {
     #[wasm_bindgen(constructor)]
     pub fn new(
-        client_diagnostic_tx: &js_sys::Function,
-        client_request_tx: &js_sys::Function,
+        client_diagnostic_tx: js_sys::Function,
+        client_notification_tx: js_sys::Function,
+        client_request_tx: js_sys::Function,
     ) -> Self {
         Self {
+            manifest: None,
             asts: BTreeMap::new(),
             client_diagnostic_tx: client_diagnostic_tx.clone(),
-            _client_request_tx: client_request_tx.clone(),
+            _client_notification_tx: client_notification_tx.clone(),
+            client_request_tx: client_request_tx.clone(),
         }
     }
 
@@ -65,13 +77,14 @@ impl ClarityLanguageServer {
     pub fn on_notification(&mut self, method: &str, params: JsValue) {
         match method {
             Initialized::METHOD => {
-                log("initialized!");
+                spawn_local(take(self).get_manifest());
+                log("> initialized!");
             }
 
             DidOpenTextDocument::METHOD => {
                 let DidOpenTextDocumentParams { text_document } = from_value(params).unwrap();
                 log(&format!("> opened: {}", text_document.uri));
-                self.get_and_send_diagnostic(&text_document)
+                self.get_and_send_diagnostic(&text_document);
             }
 
             DidCloseTextDocument::METHOD => {
@@ -90,10 +103,10 @@ impl ClarityLanguageServer {
                 } = from_value(params).unwrap();
                 log(&format!("> saved: {}", text_document.uri));
 
-                self.get_and_send_diagnostic(&TextDocumentItem {
+                let _ast = self.get_and_send_diagnostic(&TextDocumentItem {
                     uri: text_document.uri,
                     text: text.unwrap(),
-                    language_id: "clarity".into(),
+                    language_id: "clarity".to_string(),
                     version: 1,
                 });
             }
@@ -161,7 +174,7 @@ impl ClarityLanguageServer {
         }
     }
 
-    fn get_and_send_diagnostic(&mut self, text_document: &TextDocumentItem) {
+    fn get_and_send_diagnostic(&mut self, text_document: &TextDocumentItem) -> Option<ContractAST> {
         let location = FileLocation::Url {
             url: text_document.uri.clone(),
         };
@@ -174,11 +187,6 @@ impl ClarityLanguageServer {
                 let (_, (contract_id, artifacts)) = result;
                 let ast = artifacts.asts.get(&contract_id);
 
-                if ast.is_some() {
-                    self.asts
-                        .insert(text_document.uri.path().to_string(), ast.unwrap().clone());
-                }
-
                 let dst = artifacts
                     .diags
                     .iter()
@@ -188,44 +196,56 @@ impl ClarityLanguageServer {
                         acc
                     });
 
-                let data = PublishDiagnosticsParams {
-                    uri: Url::parse(&location.to_string()).unwrap(),
-                    diagnostics: dst,
-                    version: None,
+                match self.client_diagnostic_tx.call1(
+                    &JsValue::null(),
+                    &to_value(&PublishDiagnosticsParams {
+                        uri: Url::parse(&location.to_string()).unwrap(),
+                        diagnostics: dst,
+                        version: None,
+                    })
+                    .unwrap(),
+                ) {
+                    Ok(_) => (),
+                    Err(err) => log(&format!("err: {:?}", err)),
                 };
 
-                let response = self
-                    .client_diagnostic_tx
-                    .call1(&JsValue::null(), &to_value(&data).unwrap());
-
-                match response {
-                    Ok(_) => {}
-                    Err(err) => {
-                        log(&format!("err: {:?}", err));
+                match ast {
+                    Some(ast) => {
+                        self.asts
+                            .insert(text_document.uri.path().to_string(), ast.clone());
+                        Some(ast.clone())
                     }
+                    None => None,
                 }
             }
-            Err(err) => log(&format!("error: {}", err)),
+            Err(err) => {
+                log(&format!("error: {}", err));
+                None
+            }
         }
     }
 
-    #[wasm_bindgen(js_name=getManifest)]
-    pub async fn _get_manifest(self) {
-        let request = VFSRequest {
-            path: String::from("./Clarinet.toml"),
-        };
-
+    pub async fn get_manifest(mut self) {
         let req = self
-            ._client_request_tx
+            .client_request_tx
             .call2(
                 &JsValue::null(),
                 &JsValue::from("vfs/readFile"),
-                &to_value(&request).unwrap(),
+                &to_value(&VFSRequest {
+                    path: String::from("./Clarinet.toml"),
+                })
+                .unwrap(),
             )
             .unwrap();
 
         let response = JsFuture::from(js_sys::Promise::resolve(&req)).await;
 
-        log(&format!("> response: {:?}", response.unwrap()));
+        match response {
+            Ok(manifest) => {
+                self.manifest = from_value(manifest).unwrap();
+                log(&format!("manifest: {:?}", self.manifest));
+            }
+            Err(_) => (),
+        };
     }
 }
