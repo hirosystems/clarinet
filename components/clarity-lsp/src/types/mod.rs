a2f28cbd727c384e1b9f96d584261b65df3efb72
Ludo Galabru <ludo@hiro.so>
2022-06-30 17:37:23 -0400

    feat: use rely on backend::process_command

--- components/clarity-lsp/src/types/mod.rs
+++ components/clarity-lsp/src/types/mod.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone, PartialEq)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct CompletionItem {
     pub label: String,
     pub kind: CompletionItemKind,
@@ -8,7 +10,7 @@ pub struct CompletionItem {
     pub insert_text_format: InsertTextFormat,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub enum CompletionItemKind {
     Module,
     Event,
@@ -18,14 +20,14 @@ pub enum CompletionItemKind {
     TypeParameter,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub enum InsertTextFormat {
     Snippet,
     PlainText,
 }
 
 #[allow(dead_code)]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 enum Symbol {
     PublicFunction,
     ReadonlyFunction,
@@ -39,7 +41,7 @@ enum Symbol {
     NonFungibleToken,
 }
 
-#[derive(Debug, Clone, Default, PartialEq)]
+#[derive(Debug, Clone, Default, PartialEq, Deserialize, Serialize)]
 pub struct CompletionMaps {
     pub inter_contract: Vec<CompletionItem>,
     pub intra_contract: Vec<CompletionItem>,
