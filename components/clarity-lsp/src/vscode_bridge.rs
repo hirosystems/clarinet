4a3f7caeb2d5a2e60c815c43204e51e58399f6fa
Ludo Galabru <ludo@hiro.so>
2022-07-01 11:27:04 -0400

    chore: poc

--- components/clarity-lsp/src/vscode_bridge.rs
+++ components/clarity-lsp/src/vscode_bridge.rs
@@ -1,6 +1,9 @@
+extern crate console_error_panic_hook;
+use std::panic;
 use crate::backend::{self, LspRequest, LspResponse};
 use crate::state::EditorState;
 use crate::utils;
+use crate::utils::spsc::{channel, Receiver, Sender};
 use async_trait::*;
 use clarinet_files::{FileAccessor, FileLocation, PerformFileAccess};
 use js_sys::Function as JsFunction;
@@ -14,9 +17,12 @@ use lsp_types::{
 };
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::{from_value as decode_from_wasm, to_value as encode_to_wasm};
-use std::sync::mpsc::{self, channel, Sender};
+use std::cell::RefCell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::{spawn_local, JsFuture};
+
+use js_sys::Promise;
+use wasm_bindgen_futures::{future_to_promise, spawn_local, JsFuture};
 
 #[wasm_bindgen]
 extern "C" {
@@ -26,25 +32,10 @@ extern "C" {
 
 #[wasm_bindgen]
 pub struct LspVscodeBridge {
-    editor_state: EditorState,
+    editor_state: Rc<RefCell<EditorState>>,
     client_diagnostic_tx: JsFunction,
     client_notification_tx: JsFunction,
     backend_to_client_tx: JsFunction,
-    file_accessor: Box<dyn FileAccessor>,
-}
-
-/// Entry point: the function `initialize_adapter_and_start_backend` is invoked from Javascript for constructing a `LspVscodeBridge`.
-#[wasm_bindgen(js_name=initializeAdapterAndStartBackend)]
-pub fn initialize_adapter_and_start_backend(
-    client_diagnostic_tx: JsFunction,
-    client_notification_tx: JsFunction,
-    backend_to_client_tx: JsFunction,
-) -> LspVscodeBridge {
-    LspVscodeBridge::new(
-        client_diagnostic_tx,
-        client_notification_tx,
-        backend_to_client_tx,
-    )
 }
 
 #[wasm_bindgen]
@@ -55,111 +46,110 @@ impl LspVscodeBridge {
         client_notification_tx: JsFunction,
         backend_to_client_tx: JsFunction,
     ) -> LspVscodeBridge {
-        let file_accessor = VscodeFilesystemAccessor::new(backend_to_client_tx.clone());
+
+        panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+        let editor_state = Rc::new(RefCell::new(EditorState::new()));
         LspVscodeBridge {
-            editor_state: EditorState::new(),
+            editor_state,
             client_diagnostic_tx,
             client_notification_tx,
             backend_to_client_tx,
-            file_accessor: Box::new(file_accessor),
         }
     }
+}
 
-    #[wasm_bindgen(js_name=onNotification)]
-    pub async fn on_notification(mut self, method: String, params: JsValue) {
-        log("command");
-        match method.as_str() {
-            Initialized::METHOD => {
-                log("> initialized!");
-            }
-            DidOpenTextDocument::METHOD => {
-                let params: DidOpenTextDocumentParams = match decode_from_wasm(params) {
-                    Ok(params) => params,
-                    _ => return,
-                };
-                log(&format!("> opened: {}", params.text_document.uri));
 
-                let command = if let Some(contract_location) =
-                    utils::get_contract_location(&params.text_document.uri)
-                {
-                    LspRequest::ContractOpened(contract_location)
-                } else if let Some(manifest_location) =
-                    utils::get_manifest_location(&params.text_document.uri)
-                {
-                    LspRequest::ManifestOpened(manifest_location)
-                } else {
-                    log("Unsupported file opened");
-                    return;
-                };
-                let mut result = backend::process_command(
+#[wasm_bindgen(js_name=onNotification)]
+pub async fn notification_handler(bridge: LspVscodeBridge, method: String, params: JsValue) -> LspVscodeBridge {
+    
+    log("command");
+
+    let file_accessor: Box<dyn FileAccessor> = Box::new(VscodeFilesystemAccessor::new(
+        bridge.backend_to_client_tx.clone(),
+    ));
+
+    match method.as_str() {
+        Initialized::METHOD => {
+            log("> initialized!");
+        }
+        DidOpenTextDocument::METHOD => {
+            let params: DidOpenTextDocumentParams = match decode_from_wasm(params) {
+                Ok(params) => params,
+                _ => return bridge,
+            };
+            log(&format!("> opened: {}", params.text_document.uri));
+
+            let command = if let Some(contract_location) =
+                utils::get_contract_location(&params.text_document.uri)
+            {
+                LspRequest::ContractOpened(contract_location)
+            } else if let Some(manifest_location) =
+                utils::get_manifest_location(&params.text_document.uri)
+            {
+                LspRequest::ManifestOpened(manifest_location)
+            } else {
+                log("Unsupported file opened");
+                return bridge
+            };
+
+            let mut result = 
+            {
+                let mut editor_state = bridge.editor_state.borrow_mut();
+                backend::process_command(
                     command,
-                    &mut self.editor_state,
-                    Some(&self.file_accessor),
-                )
-                .await;
+                    &mut editor_state,
+                    Some(&file_accessor),
+                )   
+                .await
+            };
 
-                let mut aggregated_diagnostics = vec![];
-                let mut notification = None;
-                if let Ok(ref mut response) = result {
-                    aggregated_diagnostics.append(&mut response.aggregated_diagnostics);
-                    notification = response.notification.take();
+            let mut aggregated_diagnostics = vec![];
+            let mut notification = None;
+            if let Ok(ref mut response) = result {
+                aggregated_diagnostics.append(&mut response.aggregated_diagnostics);
+                notification = response.notification.take();
+            }
+
+            for (location, mut diags) in aggregated_diagnostics.into_iter() {
+                if let Ok(url) = Url::parse(&location.to_string()) {
+                    let value = PublishDiagnosticsParams {
+                        uri: url,
+                        diagnostics: utils::clarity_diagnostics_to_lsp_type(&mut diags),
+                        version: None,
+                    };
+
+                    let value = match encode_to_wasm(&value) {
+                        Ok(value) => value,
+                        Err(e) => {
+                            log("unable to encode value");
+                            return bridge
+                        }
+                    };
+                    return bridge;
                 }
+            }
 
-                for (location, mut diags) in aggregated_diagnostics.into_iter() {
-                    if let Ok(url) = Url::parse(&location.to_string()) {
-                        let value = PublishDiagnosticsParams {
-                            uri: url,
-                            diagnostics: utils::clarity_diagnostics_to_lsp_type(&mut diags),
-                            version: None,
-                        };
 
-                        let value = match encode_to_wasm(&value) {
-                            Ok(value) => value,
-                            Err(e) => {
-                                log("unable to encode value");
-                                return;
-                            }
-                        };
-
-                        match self.client_diagnostic_tx.call1(&JsValue::null(), &value) {
-                            Err(e) => {
-                                log("unable to publish diagnostics");
-                            }
-                            _ => {}
-                        };
-                    }
-                }
-                // TODO: display eventual notifications coming from the backend
-                // if let Some((level, message)) = notification {
-                //     self.client
-                //         .show_message(message_level_type_to_tower_lsp_type(&level), message)
-                //         .await;
-                // }
-            }
-            DidCloseTextDocument::METHOD => {
-                // See LspNativeBridge::did_close
-            }
-            DidChangeTextDocument::METHOD => {
-                // See LspNativeBridge::completion
-            }
-            DidSaveTextDocument::METHOD => {
-                // See LspNativeBridge::did_save
-            }
-            _ => log(&format!("unexpected notification ({})", method)),
+            // TODO: display eventual notifications coming from the backend
+            // if let Some((level, message)) = notification {
+            //     self.client
+            //         .show_message(message_level_type_to_tower_lsp_type(&level), message)
+            //         .await;
+            // }
         }
-    }
-
-    #[wasm_bindgen(js_name=onRequest)]
-    pub fn on_request(&mut self, method: &str, params: JsValue, _token: JsValue) -> Option<String> {
-        match method {
-            "events/v1/cursorMoved" => None,
-            "requests/v1/getAst" => None,
-            _ => {
-                log(&format!("unexpected request ({})", method));
-                None
-            }
+        DidCloseTextDocument::METHOD => {
+            // See LspNativeBridge::did_close
         }
+        DidChangeTextDocument::METHOD => {
+            // See LspNativeBridge::completion
+        }
+        DidSaveTextDocument::METHOD => {
+            // See LspNativeBridge::did_save
+        }
+        _ => log(&format!("unexpected notification ({})", method)),
     }
+    return bridge
 }
 
 #[derive(Serialize, Deserialize)]
@@ -180,6 +170,7 @@ impl VscodeFilesystemAccessor {
 #[async_trait]
 impl FileAccessor for VscodeFilesystemAccessor {
     fn read_manifest_content(&self, manifest_location: FileLocation) -> PerformFileAccess {
+        log("reading manifest");
         let path = manifest_location.to_string();
         let req = self
             .client_request_tx
@@ -207,6 +198,7 @@ impl FileAccessor for VscodeFilesystemAccessor {
         manifest_location: FileLocation,
         relative_path: String,
     ) -> PerformFileAccess {
+        log("reading contract");
         let path = relative_path.clone();
         let req = self
             .client_request_tx
