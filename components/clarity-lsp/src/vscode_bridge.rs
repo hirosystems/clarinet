04935efb7cc17751529e1b36c8ff28d845e94a27
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2022-07-13 18:28:21 +0200

    fix: use rwlock in bridge

--- components/clarity-lsp/src/vscode_bridge.rs
+++ components/clarity-lsp/src/vscode_bridge.rs
@@ -1,33 +1,29 @@
 extern crate console_error_panic_hook;
-use crate::backend::{self, LspRequest};
+use crate::backend::{self, LspRequestAsync, LspRequestSync};
 use crate::state::EditorState;
-use crate::utils::{self, log};
+use crate::utils::{self, get_contract_location, get_manifest_location, log};
 use async_trait::*;
 use clarinet_files::{FileAccessor, FileLocation, PerformFileAccess};
-use js_sys::Function as JsFunction;
+use js_sys::{Function as JsFunction, Promise};
 use lsp_types::{
-    notification::{
-        DidChangeTextDocument, DidCloseTextDocument, DidOpenTextDocument, DidSaveTextDocument,
-        Initialized, Notification,
-    },
+    notification::{DidOpenTextDocument, DidSaveTextDocument, Initialized, Notification},
     request::{Completion, Request},
     DidOpenTextDocumentParams, PublishDiagnosticsParams, Url,
 };
 use lsp_types::{CompletionParams, DidSaveTextDocumentParams};
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::{from_value as decode_from_wasm, to_value as encode_to_wasm};
-use std::cell::RefCell;
 use std::panic;
-use std::rc::Rc;
+use std::sync::{Arc, RwLock};
 
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen_futures::{future_to_promise, JsFuture};
 
 #[wasm_bindgen]
 pub struct LspVscodeBridge {
-    editor_state: Rc<RefCell<EditorState>>,
+    editor_state: Arc<RwLock<EditorState>>,
     client_diagnostic_tx: JsFunction,
-    client_notification_tx: JsFunction,
+    _client_notification_tx: JsFunction,
     backend_to_client_tx: JsFunction,
 }
 
@@ -36,214 +32,183 @@ impl LspVscodeBridge {
     #[wasm_bindgen(constructor)]
     pub fn new(
         client_diagnostic_tx: JsFunction,
-        client_notification_tx: JsFunction,
+        _client_notification_tx: JsFunction,
         backend_to_client_tx: JsFunction,
     ) -> LspVscodeBridge {
         panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-        let editor_state = Rc::new(RefCell::new(EditorState::new()));
+        let editor_state = Arc::new(RwLock::new(EditorState::new()));
         LspVscodeBridge {
             editor_state,
             client_diagnostic_tx,
-            client_notification_tx,
+            _client_notification_tx,
             backend_to_client_tx,
         }
     }
-}
 
-#[wasm_bindgen(js_name=onNotification)]
-pub async fn notification_handler(
-    bridge: LspVscodeBridge,
-    method: String,
-    params: JsValue,
-) -> LspVscodeBridge {
-    log!("> method: {}", method);
+    #[wasm_bindgen(js_name=onNotification)]
+    pub fn notification_handler(&self, method: String, params: JsValue) -> Promise {
+        log!("> notification method: {}", method);
 
-    let file_accessor: Box<dyn FileAccessor> = Box::new(VscodeFilesystemAccessor::new(
-        bridge.backend_to_client_tx.clone(),
-    ));
+        let file_accessor: Box<dyn FileAccessor> = Box::new(VscodeFilesystemAccessor::new(
+            self.backend_to_client_tx.clone(),
+        ));
 
-    match method.as_str() {
-        Initialized::METHOD => {
-            log!("> initialized!");
-        }
-        DidOpenTextDocument::METHOD => {
-            let params: DidOpenTextDocumentParams = match decode_from_wasm(params) {
-                Ok(params) => params,
-                _ => return bridge,
-            };
-            log!("> opened: {}", params.text_document.uri);
-
-            let command = if let Some(contract_location) =
-                utils::get_contract_location(&params.text_document.uri)
-            {
-                LspRequest::ContractOpened(contract_location)
-            } else if let Some(manifest_location) =
-                utils::get_manifest_location(&params.text_document.uri)
-            {
-                LspRequest::ManifestOpened(manifest_location)
-            } else {
-                log!("Unsupported file opened");
-                return bridge;
-            };
-
-            let mut result = {
-                let mut editor_state = bridge.editor_state.borrow_mut();
-                backend::process_command(command, &mut editor_state, Some(&file_accessor)).await
-            };
-
-            let mut aggregated_diagnostics = vec![];
-            let mut notification = None;
-            if let Ok(ref mut response) = result {
-                aggregated_diagnostics.append(&mut response.aggregated_diagnostics);
-                notification = response.notification.take();
+        match method.as_str() {
+            Initialized::METHOD => {
+                log!("> initialized");
             }
+            DidOpenTextDocument::METHOD => {
+                let params: DidOpenTextDocumentParams = match decode_from_wasm(params) {
+                    Ok(params) => params,
+                    _ => return Promise::resolve(&JsValue::null()),
+                };
+                let uri = &params.text_document.uri;
+                log!("> opened: {}", uri);
 
-            for (location, mut diags) in aggregated_diagnostics.into_iter() {
-                if let Ok(url) = Url::parse(&location.to_string()) {
-                    let value = PublishDiagnosticsParams {
-                        uri: url,
-                        diagnostics: utils::clarity_diagnostics_to_lsp_type(&mut diags),
-                        version: None,
-                    };
+                let command = if let Some(contract_location) = get_contract_location(&uri) {
+                    LspRequestAsync::ContractOpened(contract_location)
+                } else if let Some(manifest_location) = get_manifest_location(&uri) {
+                    LspRequestAsync::ManifestOpened(manifest_location)
+                } else {
+                    log!("Unsupported file opened");
+                    return Promise::resolve(&JsValue::null());
+                };
 
-                    let value = match encode_to_wasm(&value) {
-                        Ok(value) => value,
-                        Err(_) => {
-                            log!("unable to encode value");
-                            return bridge;
+                let editor_state = self.editor_state.clone();
+                let send_diagnostic = self.client_diagnostic_tx.clone();
+
+                return future_to_promise(async move {
+                    let mut result = match editor_state.try_write() {
+                        Ok(mut state) => {
+                            backend::process_command(command, &mut state, Some(&file_accessor))
+                                .await
                         }
+                        Err(_) => return Err(JsValue::from("unable to lock")),
                     };
 
-                    bridge
-                        .client_diagnostic_tx
-                        .call1(&JsValue::null(), &value)
-                        .unwrap();
-                }
-            }
-            return bridge;
+                    let mut aggregated_diagnostics = vec![];
+                    if let Ok(ref mut response) = result {
+                        aggregated_diagnostics.append(&mut response.aggregated_diagnostics);
+                    }
 
-            // TODO: display eventual notifications coming from the backend
-            // if let Some((level, message)) = notification {
-            //     self.client
-            //         .show_message(message_level_type_to_tower_lsp_type(&level), message)
-            //         .await;
-            // }
-        }
-        DidCloseTextDocument::METHOD => {
-            // See LspNativeBridge::did_close
-        }
-        DidChangeTextDocument::METHOD => {
-            // See LspNativeBridge::completion
-        }
+                    for (location, mut diags) in aggregated_diagnostics.into_iter() {
+                        if let Ok(url) = Url::parse(&location.to_string()) {
+                            let value = PublishDiagnosticsParams {
+                                uri: url,
+                                diagnostics: utils::clarity_diagnostics_to_lsp_type(&mut diags),
+                                version: None,
+                            };
 
-        DidSaveTextDocument::METHOD => {
-            let params: DidSaveTextDocumentParams = match decode_from_wasm(params) {
-                Ok(params) => params,
-                _ => return bridge,
-            };
-            log!("> saved: {}", params.text_document.uri);
+                            let value = match encode_to_wasm(&value) {
+                                Ok(value) => value,
+                                Err(_) => return Err(JsValue::from("unable to encode value")),
+                            };
 
-            let command = if let Some(contract_location) =
-                utils::get_contract_location(&params.text_document.uri)
-            {
-                LspRequest::ContractChanged(contract_location)
-            } else if let Some(manifest_location) =
-                utils::get_manifest_location(&params.text_document.uri)
-            {
-                LspRequest::ManifestChanged(manifest_location)
-            } else {
-                log!("Unsupported file opened");
-                return bridge;
-            };
-
-            let mut result = {
-                let mut editor_state = bridge.editor_state.borrow_mut();
-                backend::process_command(command, &mut editor_state, Some(&file_accessor)).await
-            };
-
-            let mut aggregated_diagnostics = vec![];
-            // let mut notification = None;
-            if let Ok(ref mut response) = result {
-                aggregated_diagnostics.append(&mut response.aggregated_diagnostics);
-                // notification = response.notification.take();
-            }
-
-            for (location, mut diags) in aggregated_diagnostics.into_iter() {
-                if let Ok(url) = Url::parse(&location.to_string()) {
-                    let value = PublishDiagnosticsParams {
-                        uri: url,
-                        diagnostics: utils::clarity_diagnostics_to_lsp_type(&mut diags),
-                        version: None,
-                    };
-
-                    let value = match encode_to_wasm(&value) {
-                        Ok(value) => value,
-                        Err(_) => {
-                            log!("unable to encode value");
-                            return bridge;
+                            let _ = send_diagnostic.call1(&JsValue::null(), &value);
                         }
+                    }
+                    Ok(JsValue::TRUE)
+                });
+            }
+
+            DidSaveTextDocument::METHOD => {
+                let params: DidSaveTextDocumentParams = match decode_from_wasm(params) {
+                    Ok(params) => params,
+                    _ => return Promise::resolve(&JsValue::null()),
+                };
+                let uri = &params.text_document.uri;
+                log!("> saved: {}", uri);
+                let command = if let Some(contract_location) = utils::get_contract_location(uri) {
+                    LspRequestAsync::ContractChanged(contract_location)
+                } else if let Some(manifest_location) = utils::get_manifest_location(uri) {
+                    LspRequestAsync::ManifestChanged(manifest_location)
+                } else {
+                    log!("Unsupported file opened");
+                    return Promise::resolve(&JsValue::null());
+                };
+
+                let editor_state = self.editor_state.clone();
+                let send_diagnostic = self.client_diagnostic_tx.clone();
+
+                return future_to_promise(async move {
+                    let mut result = match editor_state.try_write() {
+                        Ok(mut state) => {
+                            backend::process_command(command, &mut state, Some(&file_accessor))
+                                .await
+                        }
+                        Err(_) => return Err(JsValue::from("unable to lock")),
                     };
 
-                    bridge
-                        .client_diagnostic_tx
-                        .call1(&JsValue::null(), &value)
-                        .unwrap();
-                }
+                    let mut aggregated_diagnostics = vec![];
+                    if let Ok(ref mut response) = result {
+                        aggregated_diagnostics.append(&mut response.aggregated_diagnostics);
+                    }
+
+                    for (location, mut diags) in aggregated_diagnostics.into_iter() {
+                        if let Ok(url) = Url::parse(&location.to_string()) {
+                            let value = PublishDiagnosticsParams {
+                                uri: url,
+                                diagnostics: utils::clarity_diagnostics_to_lsp_type(&mut diags),
+                                version: None,
+                            };
+
+                            let value = match encode_to_wasm(&value) {
+                                Ok(value) => value,
+                                Err(_) => return Err(JsValue::from("unable to encode value")),
+                            };
+
+                            let _ = send_diagnostic.call1(&JsValue::null(), &value);
+                        }
+                    }
+                    Ok(JsValue::TRUE)
+                });
             }
-
-            return bridge;
-        }
-        _ => log!("unexpected notification ({})", method),
-    }
-    return bridge;
-}
-
-#[wasm_bindgen(js_name=onRequest)]
-pub async fn request_handler(
-    bridge: LspVscodeBridge,
-    method: String,
-    params: JsValue,
-) -> Option<JsValue> {
-    log!("> method: {}", method);
-    let file_accessor: Box<dyn FileAccessor> = Box::new(VscodeFilesystemAccessor::new(
-        bridge.backend_to_client_tx.clone(),
-    ));
-
-    match method.as_str() {
-        Completion::METHOD => {
-            let params: CompletionParams = match decode_from_wasm(params) {
-                Ok(params) => params,
-                _ => return None,
-            };
-            let file_url = params.text_document_position.text_document.uri;
-            log!("> completions: {}", file_url.to_string());
-            let mut editor_state = bridge.editor_state.borrow_mut();
-            log!(
-                "> editor_state: {:?}",
-                editor_state.get_aggregated_diagnostics()
-            );
-
-            let command = match utils::get_contract_location(&file_url) {
-                Some(location) => LspRequest::GetIntellisense(location),
-                _ => return None,
-            };
-
-            let result = {
-                backend::process_command(command, &mut editor_state, Some(&file_accessor)).await
-            };
-
-            match result {
-                Ok(result) => return Some(encode_to_wasm(&result.completion_items).unwrap()),
-                Err(_) => {
-                    return None;
-                }
+            _ => {
+                log!("unexpected notification ({})", method);
             }
         }
-        _ => log!("unexpected request ({})", method),
+        return Promise::resolve(&JsValue::null());
     }
 
-    return None;
+    #[wasm_bindgen(js_name=onRequest)]
+    pub fn request_handler(&self, method: String, params: JsValue) -> JsValue {
+        log!("> request method: {}", method);
+
+        match method.as_str() {
+            Completion::METHOD => {
+                let params: CompletionParams = match decode_from_wasm(params) {
+                    Ok(params) => params,
+                    _ => return JsValue::null(),
+                };
+                let file_url = params.text_document_position.text_document.uri;
+
+                let command = match utils::get_contract_location(&file_url) {
+                    Some(location) => LspRequestSync::GetIntellisense(location),
+                    _ => return JsValue::null(),
+                };
+
+                let result = match self.editor_state.try_read() {
+                    Ok(editor_state) => backend::process_command_sync(command, &editor_state),
+                    Err(_) => return JsValue::null(),
+                };
+
+                let value = match encode_to_wasm(&result.completion_items) {
+                    Ok(value) => value,
+                    Err(_) => {
+                        log!("unable to encode value");
+                        return JsValue::null();
+                    }
+                };
+                return value;
+            }
+            _ => {
+                log!("unexpected request ({})", method);
+            }
+        }
+
+        return JsValue::null();
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -276,7 +241,7 @@ impl FileAccessor for VscodeFilesystemAccessor {
             .unwrap();
 
         return Box::pin(async move {
-            let response = JsFuture::from(js_sys::Promise::resolve(&req)).await;
+            let response = JsFuture::from(Promise::resolve(&req)).await;
             match response {
                 Ok(manifest) => Ok((
                     FileLocation::from_url_string(&path).unwrap(),
@@ -309,7 +274,7 @@ impl FileAccessor for VscodeFilesystemAccessor {
             .unwrap();
 
         return Box::pin(async move {
-            let response = JsFuture::from(js_sys::Promise::resolve(&req)).await;
+            let response = JsFuture::from(Promise::resolve(&req)).await;
 
             match response {
                 Ok(manifest) => Ok((contract_location, decode_from_wasm(manifest).unwrap())),
