9699a6f38722d60fd54e892d52e8315a9d932acc
Hugo C <911307+hugocaillard@users.noreply.github.com>
2022-12-08 17:46:49 +0100

    fix: keyword matching for go to definition (#708)

--- components/clarity-lsp/src/common/requests/helpers.rs
+++ components/clarity-lsp/src/common/requests/helpers.rs
@@ -16,14 +16,15 @@ pub fn span_to_range(span: &Span) -> Range {
     )
 }
 
-pub fn is_position_within_span(position: &Position, span: &Span) -> bool {
+// end_offset is usded to include the end position of a keyword, for go to definition in particular
+pub fn is_position_within_span(position: &Position, span: &Span, end_offset: u32) -> bool {
     if position.line < span.start_line || position.line > span.end_line {
         return false;
     }
     if position.line == span.start_line && position.character < span.start_column {
         return false;
     }
-    if position.line == span.end_line && position.character > span.end_column {
+    if position.line == span.end_line && position.character > span.end_column + end_offset {
         return false;
     }
 
@@ -35,7 +36,7 @@ pub fn get_expression_name_at_position(
     expressions: &Vec<SymbolicExpression>,
 ) -> Option<ClarityName> {
     for expr in expressions {
-        if is_position_within_span(position, &expr.span) {
+        if is_position_within_span(position, &expr.span, 0) {
             if let Some(function_name) = expr.match_atom() {
                 return Some(function_name.to_owned());
             } else if let Some(expressions) = expr.match_list() {
@@ -51,7 +52,7 @@ pub fn get_atom_start_at_position(
     expressions: &Vec<SymbolicExpression>,
 ) -> Option<(u32, u32)> {
     for expr in expressions {
-        if is_position_within_span(position, &expr.span) {
+        if is_position_within_span(position, &expr.span, 1) {
             if let Some(_function_name) = expr.match_atom() {
                 return Some((expr.span.start_line, expr.span.start_column));
             } else if let Some(expressions) = expr.match_list() {
