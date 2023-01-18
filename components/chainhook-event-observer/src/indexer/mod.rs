937b7669dd05b5783fe93eda86d429f5f3099531
Ludo Galabru <ludo@hiro.so>
2023-01-18 18:44:20 -0500

    fix: error logs

--- components/chainhook-event-observer/src/indexer/mod.rs
+++ components/chainhook-event-observer/src/indexer/mod.rs
@@ -218,7 +218,6 @@ impl ChainSegment {
     }
 
     fn get_block_id(&self, block_id: &BlockIdentifier, ctx: &Context) -> Option<&BlockIdentifier> {
-        ctx.try_log(|logger| slog::info!(logger, "=> {}", self.get_relative_index(block_id)));
         match self.block_ids.get(self.get_relative_index(block_id)) {
             Some(res) => Some(res),
             None => None,
