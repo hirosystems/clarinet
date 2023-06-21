ccb9f6c77ca02de82f8f887bc3037470492fb1af
Jeff Bencin <jeff.bencin@gmail.com>
2023-06-21 17:03:23 -0400

    fix: Update `DEFAULT_SUBNET_CONTRACT_ID`, use version with correct `admin`/`miner` values

--- components/clarinet-files/src/network_manifest.rs
+++ components/clarinet-files/src/network_manifest.rs
@@ -23,7 +23,7 @@ pub const DEFAULT_POSTGRES_IMAGE: &str = "postgres:14";
 pub const DEFAULT_SUBNET_NODE_IMAGE: &str = "hirosystems/stacks-subnets:0.6.2";
 pub const DEFAULT_SUBNET_API_IMAGE: &str = "hirosystems/stacks-blockchain-api:7.2.0-beta.4";
 pub const DEFAULT_SUBNET_CONTRACT_ID: &str =
-    "ST13F481SBR0R7Z6NMMH8YV2FJJYXA5JPA0AD3HP9.subnet-v2-0-0";
+    "ST2BV6879EYSC3FC5MZWBDQJ72FY7VMSKCE6BTC22.subnet-v2-0-0";
 pub const DEFAULT_STACKS_MINER_MNEMONIC: &str = "fragile loan twenty basic net assault jazz absorb diet talk art shock innocent float punch travel gadget embrace caught blossom hockey surround initial reduce";
 pub const DEFAULT_FAUCET_MNEMONIC: &str = "shadow private easily thought say logic fault paddle word top book during ignore notable orange flight clock image wealth health outside kitten belt reform";
 pub const DEFAULT_SUBNET_MNEMONIC: &str = "twice kind fence tip hidden tilt action fragile skin nothing glory cousin green tomorrow spring wrist shed math olympic multiply hip blue scout claw";
