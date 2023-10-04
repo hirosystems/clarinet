10c26d91fd8a9407ffb88252bdd2ff310ab9625f
Hugo C <911307+hugocaillard@users.noreply.github.com>
2023-10-04 18:21:52 +0200

    refactor(sdk): run vitest-helpers tests as part of clarinet-sdk tests (#1180)

--- components/clarinet-sdk/vitest.config.js
+++ components/clarinet-sdk/vitest.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
-    include: ["./tests/**/*.test.ts"],
+    include: ["./tests/**/*.test.ts", "./vitest-helpers/tests/**/*.test.ts"],
   },
 });
