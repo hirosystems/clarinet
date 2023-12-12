cdf408cd18a32c9a2ce1a193d3153b5f33a3cfb5
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2023-12-12 15:55:46 +0100

    chore: upgrade dependencies

--- components/clarity-repl/src/repl/datastore.rs
+++ components/clarity-repl/src/repl/datastore.rs
@@ -283,7 +283,7 @@ impl ClarityBackingStore for Datastore {
         panic!("Datastore cannot get_metadata_manual")
     }
 
-    #[cfg(not(target_family = "wasm"))]
+    #[cfg(not(feature = "wasm"))]
     fn get_side_store(&mut self) -> &::clarity::rusqlite::Connection {
         panic!("Datastore cannot get_side_store")
     }
