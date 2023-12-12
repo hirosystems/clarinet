ca2ed5b2be5df1a5126f1c89cf92c0bc978b5ccd
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2023-12-12 16:47:49 +0100

    refactor: cargo fmt

--- components/clarinet-files/src/network_manifest.rs
+++ components/clarinet-files/src/network_manifest.rs
@@ -807,9 +807,7 @@ impl NetworkManifest {
                 miner_coinbase_recipient: devnet_config
                     .miner_coinbase_recipient
                     .unwrap_or(miner_stx_address),
-                miner_wallet_name: devnet_config
-                    .miner_wallet_name
-                    .unwrap_or("".to_string()),
+                miner_wallet_name: devnet_config.miner_wallet_name.unwrap_or("".to_string()),
                 faucet_btc_address,
                 faucet_stx_address,
                 faucet_mnemonic,
