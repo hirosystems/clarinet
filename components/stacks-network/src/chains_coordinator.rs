e5e6003cccff912395a372f4a2c604cf1d8c6e7f
Hugo C <911307+hugocaillard@users.noreply.github.com>
2024-01-17 19:50:50 +0100

    fix: typo
    Co-authored-by: Micaiah Reid <micaiahreid@gmail.com>

--- components/stacks-network/src/chains_coordinator.rs
+++ components/stacks-network/src/chains_coordinator.rs
@@ -658,7 +658,7 @@ pub async fn publish_stacking_orders(
                         }
                         Err(e) => {
                             let _ = devnet_event_tx
-                                .send(DevnetEvent::error(format!("unable to stack: {}", e)));
+                                .send(DevnetEvent::error(format!("Unable to stack: {}", e)));
                         }
                     }
                 };
