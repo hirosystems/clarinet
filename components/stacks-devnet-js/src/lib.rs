d231fb8b7ccb523d61316713b750d713772648f0
MicaiahReid <micaiahreid@gmail.com>
2024-01-25 12:16:26 -0500

    fix build and ui pox info

--- components/stacks-devnet-js/src/lib.rs
+++ components/stacks-devnet-js/src/lib.rs
@@ -746,6 +746,8 @@ impl StacksDevnet {
                     wallet,
                     slots,
                     btc_address,
+                    auto_extend: Some(false),
+                    cycles_stacked: None,
                 });
             }
             overrides.pox_stacking_orders = Some(stacking_orders);
