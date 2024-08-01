e5f5f795a2f340daa0a8fedb2aeea2048ccd9b1f
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2024-08-01 12:07:25 +0200

    refactor: remove test devnet logs

--- components/stacks-network/src/chains_coordinator.rs
+++ components/stacks-network/src/chains_coordinator.rs
@@ -437,11 +437,7 @@ pub async fn start_chains_coordinator(
             }
             ObserverEvent::NotifyBitcoinTransactionProxied => {
                 if !boot_completed.load(Ordering::SeqCst) {
-                    let _ = devnet_event_tx
-                        .send(DevnetEvent::info("1 - Waiting for boot mining".to_string()));
                     std::thread::sleep(std::time::Duration::from_secs(1));
-                    let _ = devnet_event_tx
-                        .send(DevnetEvent::info("1 - Trigger boot mining".to_string()));
                     let res = mine_bitcoin_block(
                         &config.services_map_hosts.bitcoin_node_host,
                         config.devnet_config.bitcoin_node_username.as_str(),
