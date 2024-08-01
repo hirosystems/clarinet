a3e90023dc6fa9b38ba5017c7a79024e1d011863
Hugo C <911307+hugocaillard@users.noreply.github.com>
2024-08-01 12:06:54 +0200

    feat: auto extend stacking_orders in devnet config by default (#1528)

--- components/clarinet-cli/src/generate/project.rs
+++ components/clarinet-cli/src/generate/project.rs
@@ -354,24 +354,28 @@ disable_stacks_api = false
 # Send some stacking orders
 [[devnet.pox_stacking_orders]]
 start_at_cycle = 1
-duration = 12
+duration = 10
+auto_extend = true
 wallet = "wallet_1"
 slots = 2
 btc_address = "mr1iPkD9N3RJZZxXRk7xF9d36gffa6exNC"
 
 [[devnet.pox_stacking_orders]]
 start_at_cycle = 1
-duration = 12
+duration = 10
+auto_extend = true
 wallet = "wallet_2"
 slots = 1
 btc_address = "muYdXKmX9bByAueDe6KFfHd5Ff1gdN9ErG"
 
 [[devnet.pox_stacking_orders]]
 start_at_cycle = 1
-duration = 12
+duration = 10
+auto_extend = true
 wallet = "wallet_3"
 slots = 1
 btc_address = "mvZtbibDAAA3WLpY7zXXFqRa3T4XSknBX7"
+
 "#,
             default_derivation_path = DEFAULT_DERIVATION_PATH,
             default_bitcoin_node_image = DEFAULT_BITCOIN_NODE_IMAGE,
