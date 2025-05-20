abfe1694a042b291928937e0ecf1bf469012900a
brady.ouren <bouren@hiro.so>
2025-05-20 11:47:31 -0700

    mine 1 bitcoin block to jump start the stacks node

--- components/stacks-network/src/orchestrator.rs
+++ components/stacks-network/src/orchestrator.rs
@@ -2807,8 +2807,40 @@ events_keys = ["*"]
             }
         } else {
             let _ = devnet_event_tx.send(DevnetEvent::info(
-                "Using cached blockchain data - skipping initial block generation".to_string(),
+                "Using cached blockchain data - mining one block".to_string(),
             ));
+
+            loop {
+                let rpc_call = base_builder(
+                    &bitcoin_node_url,
+                    &devnet_config.bitcoin_node_username,
+                    &devnet_config.bitcoin_node_password,
+                )
+                .json(&json!({
+                "jsonrpc": "1.0",
+                "id": "stacks-network",
+                "method": "generatetoaddress",
+                "params": [json!(1), json!(miner_address)]
+                }))
+                .send()
+                .await
+                .map_err(|e| format!("unable to send 'generatetoaddress' request ({})", e));
+
+                match rpc_call {
+                    Ok(_r) => break,
+                    Err(e) => {
+                        error_count += 1;
+                        if error_count > max_errors {
+                            return Err(e);
+                        } else if error_count > 1 {
+                            let _ = devnet_event_tx.send(DevnetEvent::error(e));
+                        }
+                    }
+                }
+                std::thread::sleep(std::time::Duration::from_secs(1));
+                let _ =
+                    devnet_event_tx.send(DevnetEvent::info("Waiting for bitcoin-node".to_string()));
+            }
         }
 
         let mut error_count = 0;
